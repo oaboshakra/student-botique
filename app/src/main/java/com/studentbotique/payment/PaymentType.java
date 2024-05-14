53241db5369ccd6277986aab89af50fff4ac0f8e
oaboshakra <os725191@dal.ca>
2024-05-14 19:22:24 -0400

    Implemented logic for the GooglePay, PaymentType, and the SmartPay adapter.

--- app/src/main/java/com/studentbotique/payment/PaymentType.java
+++ app/src/main/java/com/studentbotique/payment/PaymentType.java
@@ -1,4 +1,14 @@
 package com.studentbotique.payment;
+import com.studentbotique.constants.AppConstants;
 
 public class PaymentType {
+
+    String type;
+
+    public final static PaymentType GooglePayment = new PaymentType(AppConstants.GOOGLE_PAY);
+    public final static PaymentType SchoolPayment = new PaymentType(AppConstants.SCHOOL_CARD);
+
+    public PaymentType(String paymentType) {
+        this.type = paymentType;
+    }
 }
