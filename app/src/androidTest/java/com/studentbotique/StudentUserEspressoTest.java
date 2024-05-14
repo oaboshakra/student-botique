c5df07503ed098f1ce0d191d0eb025b37ec551a3
oaboshakra <os725191@dal.ca>
2024-05-13 23:25:12 -0400

    Developed espresso test case for students with a silver membership.

---
+++ app/src/androidTest/java/com/studentbotique/StudentUserEspressoTest.java
@@ -0,0 +1,32 @@
+package com.studentbotique;
+
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.is;
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import org.junit.runner.RunWith;
+import org.junit.Test;
+import com.studentbotique.ui.LandingActivity;
+@RunWith(AndroidJUnit4.class)
+public class StudentUserEspressoTest {
+
+    ActivityScenario<LandingActivity> activityScenario;
+
+    @Test
+    public void testStudentWithSilverMembership() {
+        activityScenario = ActivityScenario.launch(LandingActivity.class);
+        onView(withId(R.id.depositBox)).perform(typeText("200"));
+        onView(withId(R.id.membershipSpinner)).perform(click());
+        onData(is("Silver"))).perform(click());
+        onView(withId(R.id.calculateDiscountButton)).perform(click());
+        onView(withId(R.id.totalDiscountLabel)).check(matches(withText("$20.0")));
+    }
+
+}
---
+++ app/src/main/java/com/studentbotique/ui/LandingActivity.java
@@ -0,0 +1,6 @@
+package com.studentbotique.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LandingActivity extends AppCompatActivity{
+}
