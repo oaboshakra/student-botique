c01246d6962b553a4bed7a95fd5e633827b22681
oaboshakra <os725191@dal.ca>
2024-05-14 09:29:33 -0400

    Added the setup method for the PaymentUI test to prepare the testing environment.

---
+++ app/src/androidTest/java/com/studentbotique/PaymentUITest.java
@@ -0,0 +1,32 @@
+package com.studentbotique;
+
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
+
+import static org.junit.Assert.assertTrue;
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.uiautomator.By;
+import androidx.test.uiautomator.UiDevice;
+import androidx.test.uiautomator.Until;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PaymentUITest {
+
+    public String launcherPackage = "com.studentbotique";
+    public int TIME_OUT = 5000;
+    UiDevice device;
+
+    @Before
+    public void setup() {
+        device = UiDevice.getInstance(getInstrumentation());
+        Context context = ApplicationProvider.getApplicationContext();
+        Intent launcherIntent = context.getPackageManager().getLaunchIntentForPackage(launcherPackage);
+        launcherIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        context.startActivity(launcherIntent);
+        device.wait(Until.hasObject(By.pkg(launcherPackage).depth(0)), TIME_OUT);
+    }
+}
