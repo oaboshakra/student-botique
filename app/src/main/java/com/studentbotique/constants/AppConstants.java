366f3e992406964941e3708933ebe09153f721ea
oaboshakra <os725191@dal.ca>
2024-05-14 13:44:16 -0400

    Added some constants.

--- app/src/main/java/com/studentbotique/constants/AppConstants.java
+++ app/src/main/java/com/studentbotique/constants/AppConstants.java
@@ -1,6 +1,14 @@
 package com.studentbotique.constants;
 
 public class AppConstants {
+
+    public static final String CLOTHES = "Clothes";
+    public static final String BOOK = "Book";
+
+    public static final String SILVER_MEMBERSHIP = "Silver";
+    public static final String GOLD_MEMBERSHIP = "Gold";
+    public static final String PLATINUM_MEMBERSHIP = "Platinum";
+
     public static final double SILVER_RATE = 0.10;
     public static final double GOLD_RATE = 0.15;
     public static final double PLATINUM_RATE = 0.20;
@@ -12,7 +20,18 @@ public class AppConstants {
     public static final String DA_VINCI_CODE = "Da Vinci Code";
     public static final String SHERLOCK_HOLMES = "Sherlock Holmes";
 
+    public static final String INITIAL_LOYALTY = "INITIAL_LOYALTY";
     public static final int INITIAL_LOYALTY_POINTS = 2500;
     public static final String TRANSACTION_SUCCESSFUL = "Transaction successful";
     public static final String TRANSACTION_FAILED = "Transaction failed";
+
+    public static final String SCHOOL_CARD = "School Card";
+    public static final String GOOGLE_PAY = "Google Pay";
+    public static final double GOOGLE_CREDIT = 100;
+    public static final String SMART_PAY = "Smart Pay";
+    public static final String SCHOOL_CARD_BALANCE = "SCHOOL_CARD_BALANCE";
+
+    public static final String BUY_BOOK = "Buy Book";
+    public static final String CASH_IN = "Cash In";
+    public static final String TRANSFER = "Transfer";
 }
