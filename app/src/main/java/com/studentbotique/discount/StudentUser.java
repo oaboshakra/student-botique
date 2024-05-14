31b3dae1e94d8e073d3a51c7eef781032bb157de
oaboshakra <os725191@dal.ca>
2024-05-14 14:58:19 -0400

    Implemented StudentUser and Membership logic.

--- app/src/main/java/com/studentbotique/discount/StudentUser.java
+++ app/src/main/java/com/studentbotique/discount/StudentUser.java
@@ -1,4 +1,42 @@
 package com.studentbotique.discount;
 
-public class StudentUser {
+public class StudentUser implements IStudent{
+
+    String firstName;
+    String lastName;
+    IWallet wallet;
+    Membership membership;
+
+    public StudentUser(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public void setWallet(IWallet wallet) {
+        this.wallet = wallet;
+    }
+
+    @Override
+    public IWallet getWallet() {
+        return this.wallet;
+    }
+
+    public void setMembership(Membership membership) {
+        this.membership = membership;
+    }
+
+    public Membership getMembership() {
+        return this.membership;
+    }
+
+
+    @Override
+    public double calculateDiscount() {
+        if (membership != null) {
+            return membership.getRule().calculate(this);
+        } else {
+            return 0;
+        }
+    }
 }
