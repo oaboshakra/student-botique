74e7844d1f84118cb47e4b13be67dc972d8d2041
oaboshakra <os725191@dal.ca>
2024-05-14 20:45:42 -0400

    Implemented SmartPay logic.

--- app/src/main/java/com/studentbotique/payment/SmartPay.java
+++ app/src/main/java/com/studentbotique/payment/SmartPay.java
@@ -1,4 +1,47 @@
 package com.studentbotique.payment;
 
-public class SmartPay {
+public class SmartPay implements ISchoolCard {
+
+    public SmartPayAdapter adapter;
+    double balance;
+    double paidAmount;
+
+    public SmartPay(SmartPayAdapter adapter) {
+        this.adapter = adapter;
+    }
+
+    public void setSchoolBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getSchoolBalance() {
+        return this.balance;
+    }
+
+    @Override
+    public boolean pay(PaymentType paymentType, double amount) {
+        if (paymentType.equals(PaymentType.SchoolPayment)) {
+            if (adapter != null) {
+                boolean paid = adapter.pay(paymentType, amount);
+                if (paid) {
+                    paidAmount += amount;
+                }
+            }
+        } else if (paymentType.equals(PaymentType.GooglePayment)) {
+            if (adapter != null) {
+                boolean paid = adapter.pay(paymentType, amount);
+                if (paid) {
+                    paidAmount += amount;
+                }
+                return paid;
+            } else {
+                return false;
+            }
+        }
+        return false;
+    }
+
+    public double getPaidAmount() {
+        return this.paidAmount;
+    }
 }
